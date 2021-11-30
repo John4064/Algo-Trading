6a30044b2f81892a783d2d50afab67a0950e2629
John Parkhurst <jparkhurst120@gmail.com>
2021-11-30 01:32:57 -0500

    Updating Test Functions
    Updating and implementing unit testing to web scrape
    Currently check types of return values as well as certain values are foound on yahoo page. Issue is file directory is invalid need to refactor tmmrw morning.

--- tests/algotest.py
+++ tests/algotest.py
@@ -1,10 +1,19 @@
 import unittest
+from scrape import *
 
-
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
-
+class Webscrape(unittest.TestCase):
+    def checkCont(self):
+        """
+        Will make sure desired content is found on yahoo page
+        """
+        self.assertTrue(type(1) is int)
+    def returnVal(self):
+        """
+        Will Test for if successfuly returning lists list when value
+        """
+        self.scrape = YahooScrape()
+        self.scrape.findVolatile(100)
+        self.assertIsInstance(self.scrape, list)
 
 if __name__ == '__main__':
     unittest.main()
