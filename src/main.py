64b3f64709e7006e2f487f9b58c4fbdae3a2c086
John Parkhurst <jparkhurst120@gmail.com>
2022-03-16 00:47:09 -0400

    Refactor

--- src/main.py
+++ src/main.py
@@ -1,30 +1,27 @@
 import alpaca_trade_api as alpaca
 from algorithm import *
-from indicators import *
 import time
 import sys
 from config import *
+
+
 def run():
     """
     To Test importing tickers
     """
     ss = YahooScrape()
-    #voldf is the voltaile dataframe contains all info
-    #sort valid creates valid stocks which is just a list of tickers
+    # voldf is the volatile dataframe contains all info
+    # sort valid creates valid stocks which is just a list of tickers
     try:
         ss.findVolatile(100)
-        #Returns the tickers
+        # Returns the tickers
         print(ss.vol)
         return ss.sortValid(1)
     except:
         print("For heading in tr_elements[0] line 34 find volatile")
     return []
-def test():
-    unittest.main()
-    return
+
+
 if __name__ == '__main__':
-    tickers =['SKLZ', 'T', 'NKLA', 'FSR', 'TDC', 'HPE', 'HBAN', 'CS', 'CHPT', 'DOW', 'XM', 'NUAN', 'YSG', 'WBT']
+    tickers = ['SKLZ', 'T', 'NKLA', 'FSR', 'TDC', 'HPE', 'HBAN', 'CS', 'CHPT', 'DOW', 'XM', 'NUAN', 'YSG', 'WBT']
     myApi = algo()
-
-
-
