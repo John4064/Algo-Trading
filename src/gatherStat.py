34e5f5c80daf25ab7991d728f7ecfdd79bc6b55e
John Parkhurst <jparkhurst120@gmail.com>
2022-03-16 22:52:58 -0400

    Processing bar

--- src/gatherStat.py
+++ src/gatherStat.py
@@ -1,9 +1,16 @@
 from config import *
-import alpaca_trade_api as alpaca
+from alpaca_trade_api.rest import REST, TimeFrame
 import pandas as pd
 
 if __name__ == '__main__':
-    api = alpaca.REST(TESTAPI_KEY, TESTAPI_SECRET, APCA_API_BASE_URL, 'v2')
+    api = REST(TESTAPI_KEY, TESTAPI_SECRET, APCA_API_BASE_URL, 'v2')
 
-    api.get_bars("AAPL", TimeFrame.Hour, "2021-06-08", "2021-06-08", adjustment='raw').df
 
+    def process_bar(bar):
+        # process bar
+        print(bar)
+
+
+    bar_iter = api.get_bars_iter("UNH", TimeFrame.Hour, "2021-06-08", "2021-06-08", adjustment='raw')
+    for bar in bar_iter:
+        process_bar(bar)
